e4b3127937ee9c209548b51604f742e729fed113
kingstar1278 <82220514+kingstar1278@users.noreply.github.com>
2021-08-21 05:55:52 +0100

    Update post.js

--- models/post.js
+++ models/post.js
@@ -1,47 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose  = require("mongoose")
 
 const postSchema = mongoose.Schema({
-    phone: {
-        type: String,
-        required: true
-    },
-    brand: {
-        type: String,
-        required: true
-    },
-    memoryStorageCapacity: {
-        type:  String,
-        required: true
-    },
-    ram: {
-        type: String,
-        required: true
-    },
-    formFactor: {
-        type: String,
-        required: true
-    },
-    os: {
-        type: String,
-        required: true
-    },
-    color: {
-        type: String,
-        required: true
-    },
-    displaySize: {
-        type: String,
-        required: true
-    },
-    otherCameraFeatures: {
+    gun: {
         type: String,
         required: true
     },
     price: {
         type: String,
         required: true
-    }
+    },
+    
+    bulletused: {
+        type: String,
+        required: true
+    },
+    
+ 
+    
 });
-
-
-module.exports = mongoose.model('posts', postSchema);
+module.exports = mongoose.model('posts', postSchema)
